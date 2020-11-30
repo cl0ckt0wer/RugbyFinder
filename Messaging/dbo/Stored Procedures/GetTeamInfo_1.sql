0dc08672ac5d231db820e00a654322c3190efc91
jason triplett <clocktower_hlp@hotmail.com>
2020-11-30 18:50:08 -0500

    Adding missing stored procs

---
+++ Messaging/dbo/Stored Procedures/GetTeamInfo_1.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[GetTeamInfo]
+	@id uniqueidentifier
+AS
+	SELECT t.Id as [TeamId], t.TeamBio, t.TeamCityId, t.TeamName
+	FROM DBO.Teams t
+	WHERE Id = @id
+
+RETURN 1
---
+++ Messaging/dbo/Stored Procedures/Proc_SetRuggerTeam_1.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[Proc_SetRuggerTeam]
+	@RuggerId UNIQUEIDENTIFIER,
+	@TeamId UNIQUEIDENTIFIER
+AS
+	DELETE DBO.RuggerTeam
+	WHERE RuggerId = @RuggerId;
+
+	INSERT DBO.RuggerTeam (TeamId, RuggerId)
+	VALUES (@TeamId, @RuggerId)
+
+RETURN 1
