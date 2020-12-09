1d4ff18579cbab408337577148aa4c03d519c8ad
Triplett, Jason <jason.triplett@igt.com>
2020-12-08 22:38:49 -0800

    messaging stuff

---
+++ BlazorApp2/Shared/RuggerMessageModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorApp2.Shared
+{
+    public class RuggerMessageModel
+    {
+        public Guid Other { get; set; } = Guid.Empty;
+
+        public List<string> MyMessage { get; set; } = new List<string>();
+    }
+}
---
+++ BlazorApp2/Shared/RuggerMessagePostArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorApp2.Shared
+{
+    public class RuggerMessagePostArgs
+    {
+        public Guid MyGuid { get; set; } = Guid.Empty;
+        public Guid TheirGuid { get; set; } = Guid.Empty;
+    }
+}
