3759b68fbeae66daea5271a67c18dca42e707799
Triplett, Jason <jason.triplett@igt.com>
2020-12-14 17:34:00 -0800

    Messages dev

---
+++ BlazorApp2/Server/Controllers/InboxController.cs
@@ -0,0 +1,38 @@
+﻿using BlazorApp2.Shared;
+using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp2.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class InboxController : Controller
+    {
+      
+        private SqlConnectionStringBuilder _cstring;
+      
+        public InboxController(IConfiguration configuration)
+        {
+            _cstring = new SqlConnectionStringBuilder(configuration.GetConnectionString("MessagingDatabase"));
+        }
+        [HttpGet]
+        [Route("{myguid:guid}")]
+        public async Task<IEnumerable<InboxItem>> GetInboxAsync(Guid myguid)
+        {
+            var sql = "Proc_GetInbox";
+            using(var conn = new SqlConnection(_cstring.ConnectionString))
+            {
+                IEnumerable<InboxItem> r = await conn.QueryAsync<InboxItem>(sql, new { id  = myguid }, commandType: System.Data.CommandType.StoredProcedure);
+                return r ;
+            }
+        }
+
+
+    }
+}
---
+++ Messaging/dbo/Stored Procedures/Proc_GetInbox.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[Proc_GetInbox]
+	@id UNIQUEIDENTIFIER
+AS
+	SELECT m.[From] as Id, MAX(M.SentDate) as LastMessage, COUNT(M.SentDate) as MessageCount,
+		(SELECT TOP 1 [Name] FROM dbo.RuggerName n WHERE N.Id = M.[From]) AS Name
+	FROM dbo.Messages M
+	WHERE M.[To] = @id
+	GROUP BY M.[From]
+	ORDER BY MAX(M.SentDate) DESC
+
+RETURN 0
