cae1f0fc2d61aa6f26cbdbd2179afb5362098a4b
Triplett, Jason <jason.triplett@igt.com>
2020-12-29 21:49:52 -0800

    Add app update button

--- BlazorApp2/Client/wwwroot/service-worker.published.js
+++ BlazorApp2/Client/wwwroot/service-worker.published.js
@@ -5,7 +5,12 @@ self.importScripts('./service-worker-assets.js');
 self.addEventListener('install', event => event.waitUntil(onInstall(event)));
 self.addEventListener('activate', event => event.waitUntil(onActivate(event)));
 self.addEventListener('fetch', event => event.respondWith(onFetch(event)));
-
+self.addEventListener('message', (event) => {
+    if (event.data && event.data.type === 'skipWaiting') {
+        console.log("reloaded message received");
+        skipWaiting();
+    }
+});
 const cacheNamePrefix = 'offline-cache-';
 const cacheName = `${cacheNamePrefix}${self.assetsManifest.version}`;
 const offlineAssetsInclude = [ /\.dll$/, /\.pdb$/, /\.wasm/, /\.html/, /\.js$/, /\.json$/, /\.css$/, /\.woff$/, /\.png$/, /\.jpe?g$/, /\.gif$/, /\.ico$/, /\.blat$/, /\.dat$/ ];
