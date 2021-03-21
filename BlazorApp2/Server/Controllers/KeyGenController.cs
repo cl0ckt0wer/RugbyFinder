35f0939525c3305e2a9e9304be3c3af468cfebc8
Triplett, Jason <jason.triplett@igt.com>
2021-03-20 23:55:07 -0700

    register new api key action

---
+++ BlazorApp2/Server/Controllers/KeyGenController.cs
@@ -0,0 +1,41 @@
+﻿using BlazorApp2.Shared.Model.Return;
+using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+
+namespace BlazorApp2.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class KeyGenController : Controller
+    {
+        private SqlConnectionStringBuilder _cstring;
+        public KeyGenController(IConfiguration configuration)
+        {
+            _cstring = new SqlConnectionStringBuilder(configuration.GetConnectionString("MessagingDatabase"));
+        }
+      
+        public async Task<KeyGenReturnModel> GetNewKeyAsync()
+        {
+            var key = new byte[32];
+            using (var generator = RandomNumberGenerator.Create())
+                generator.GetBytes(key);
+            string apiKey = Convert.ToBase64String(key);
+            var sql = "Proc_RegisterNewKey";
+            using (var conn = new SqlConnection(_cstring.ConnectionString))
+            {
+               
+                    var keyGenReturnModel = await conn.QueryFirstAsync<KeyGenReturnModel>(sql, new { apiKey  }
+                        , commandType: CommandType.StoredProcedure).ConfigureAwait(false);
+                return keyGenReturnModel;
+            }
+        }
+    }
+}
---
+++ Messaging/dbo/Stored Procedures/Proc_RegisterNewKey.sql
@@ -0,0 +1,7 @@
+﻿CREATE PROCEDURE [dbo].[Proc_RegisterNewKey]
+	@apikey varchar(64)
+AS
+	INSERT dbo.KeyGuid (Id, [Key])
+	OUTPUT inserted.Id AS MyGuid, inserted.[Key]
+	VALUES (NEWID(), @apikey)
+
